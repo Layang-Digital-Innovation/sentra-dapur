c9705948648d1511d47e6c1442e652c367b81a5c
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2026-03-28 23:29:31 +0700

    fix : migration

---
+++ backend/prisma/migrations/20260328162911_add_dapurunit_optional/migration.sql
@@ -0,0 +1,8 @@
+-- AlterEnum
+ALTER TYPE "BillingPeriod" ADD VALUE 'TWO_YEARS';
+
+-- AlterEnum
+ALTER TYPE "PaymentStatus" ADD VALUE 'AWAITING_APPROVAL';
+
+-- AlterEnum
+ALTER TYPE "SubscriptionPlan" ADD VALUE 'GOLD_TWO_YEARS';
